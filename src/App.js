d166998542403ce3bf0ec7bccbf95a7bb2665c90
srivinya.godavarthi <srivinya.godavarthi@vecna.com>
2020-06-17 07:54:21 -0400

    Blog UI Mini Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Link, Route } from 'react-router-dom' //npm install react-router-dom
+import Home from './components/Home'
+import Users from './components/Users'
+import Posts from './components/Posts'
+import UserShow from './components/UserShow'
+import PostShow from './components/PostShow'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                
+                <Link to="/">Home</Link> | 
+                <Link to="/users"> Users</Link> | 
+                <Link to="/posts"> Posts</Link>
+
+                <Route path="/" component={Home} exact={true}></Route>
+                <Route path="/users" component={Users} exact={true}></Route>
+                <Route path="/users/:id" component={UserShow} ></Route>
+                <Route path="/posts" component={Posts} exact={true}></Route>
+                <Route path="/posts/:id" component={PostShow}></Route>
+            </div>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
